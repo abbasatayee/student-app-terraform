ba9fd6ca1fe3127fc67925686e5d4b33851e96fb
Abbas Ataie <abbas.ataie.montazer@gmail.com>
2025-12-14 16:08:27 +0100

    Add initial Terraform configuration files for AWS infrastructure
    - Created .gitignore to exclude Terraform state files and local environment variables.
    - Added rds.tf for RDS instance and subnet group configuration.
    - Introduced security.tf for web and database security groups.
    - Defined variables in variables.tf for AWS region, VPC CIDR, subnets, and database credentials.
    - Specified Terraform version and AWS provider in versions.tf.
    - Configured VPC, subnets, internet gateway, and route tables in vpc.tf.

---
+++ rds.tf
@@ -0,0 +1,24 @@
+# rds.tf
+resource "aws_db_instance" "mysql" {
+  identifier           = "student-database"
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  multi_az             = true
+  publicly_accessible  = false
+  tags = {
+    Name = "Student Database"
+  }
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "db-subnet-group"
+  subnet_ids = [for s in aws_subnet.private : s.id]
+  description = "Private subnets for RDS"
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "db_username" {
+  default = "nodeapp"
+}
+
+variable "db_name" {
+  default = "STUDENTS"
+}
+
+variable "db_password" {
+    default = "student12"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair Name"
+  default     = "webapp-key"
+}
